119d6264b9f3af80e84cbcf04a621c4428c6130c
Artem <ma.evgenyevich@gmail.com>
2024-02-08 11:53:13 +0300

    chart

--- src/components/SectionChart.vue
+++ src/components/SectionChart.vue
@@ -10,33 +10,38 @@
         </template>
 
         <template #content>
-            <canvas class="max-h-[500px]" id="salesChart"></canvas>
+            <canvas id="chart" class="lg:max-h-[500px]"></canvas>
         </template>
     </UiSection>
 </template>
 
 <script setup>
 import Chart from 'chart.js/auto';
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 
 import UiSection from '@/components/UiSection.vue';
 import UiIcon from '@/components/UiIcon.vue';
 
+const chart = ref(null);
 const labels = ['01 Apr', '02 Apr', '03 Apr', '04 Apr', '05 Apr', '06 Apr', '07 Apr'];
 
 const data = {
     labels: labels,
     datasets: [{
         label: 'Sales',
-        data: [80, 100, 80, 100, 120, 100, 120],
+        data: [80, 120, 70, 130, 90, 100, 170],
         fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
+        borderColor: '#0b9f6e',
+        tension: 0.6,
     }]
 };
 
 const options = {
     responsive: true,
+    interaction: {
+        intersect: false,
+        mode: 'index',
+    },
     plugins: {
         legend: {
             display: false,
@@ -47,9 +52,16 @@ const options = {
             ticks: {
                 font: {
                     size: 14,
-                    weight: 600,
+                    weight: 500,
                 },
-            }
+            },
+            grid: {
+                display: false,
+                tickLength: 15,
+            },
+            border: {
+                display: false,
+            },
         },
 
         y: {
@@ -59,9 +71,25 @@ const options = {
                 stepSize: 40,
                 font: {
                     size: 14,
-                    weight: 600,
+                    weight: 500,
+                },
+                callback: function (value, index, ticks) {
+                    return value + 'K';
                 },
             },
+            grid: {
+                tickLength: 55,
+            },
+            border: {
+                display: false,
+            },
+        },
+    },
+    elements: {
+        point: {
+            backgroundColor: '#ffffff',
+            borderWidth: 2,
+            borderColor: '#0b9f6e',
         },
     },
 };
@@ -73,10 +101,14 @@ const config = {
 };
 
 onMounted(() => {
-    new Chart(
-        document.getElementById('salesChart'),
+    chart.value = new Chart(
+        document.getElementById('chart'),
         config,
     );
+});
+
+window.addEventListener('resize', () => {
+    chart.value.resize();
 });
 </script>
 
